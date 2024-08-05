e43ac1d5c5b4831eaead9b33958d5e5115daeb01
bimantaratirta <bimantarats@gmail.com>
2024-08-05 18:22:09 +0700

    fix: splash on init

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -6,8 +6,8 @@ import '../../../utils/location_service.dart';
 class SplashController extends GetxController {
   @override
   void onInit() async {
-    await Future.delayed(const Duration(seconds: 2));
-    await LocationService.getCurrentLocation();
+    await Future.delayed(const Duration(seconds: 1));
+    LocationService.getCurrentLocation(isOnInit: true);
     Get.offAllNamed(Routes.HOME);
     super.onInit();
   }
